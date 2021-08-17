a1b34c70d6bd1419e0d850d2a53be85681d5ba15
Paulo Santana <paulo.santana.r@gmail.com>
2021-08-17 11:33:17 -0300

    feat: create a simple RESTful hello world

--- Makefile
+++ Makefile
@@ -2,29 +2,34 @@ NAME := intra_profiler
 
 SRC_DIR := ./src
 OBJ_DIR := ./obj
+INCLUDE_DIR := ./includes
 
 SRC_FILES := index.c
+DEPS = ./dependencies/mongoose.c
 
 SRC := $(addprefix $(SRC_DIR)/, $(SRC_FILES))
 
 OBJ_FILES := $(SRC_FILES:.c=.o)
 OBJ := $(addprefix $(OBJ_DIR)/, $(OBJ_FILES))
 
-CFLAGS = -Wall -Werror -Wextra
+CFLAGS = -Wall -Werror -Wextra -g3
 CC := clang $(CFLAGS)
 
 RM := rm -rf
 
 all: $(NAME)
 
-$(NAME): $(OBJ_DIR) $(OBJ)
-	$(CC) $(OBJ) -o $(NAME)
+$(NAME): $(OBJ_DIR) $(DEPS) $(OBJ)
+	$(CC) $(OBJ) $(DEPS) -o $(NAME)
 
 $(OBJ_DIR):
 	@mkdir -p $(OBJ_DIR)
 
 $(OBJ_DIR)/%.o: $(SRC_DIR)/%.c
-	$(CC) -c $< -o $@ 
+	$(CC) -I$(INCLUDE_DIR) -c $< -o $@ 
+
+run: all
+	./intra_profiler
 
 clean:
 	$(RM) $(OBJ)
--- src/index.c
+++ src/index.c
@@ -1,7 +1,27 @@
+#include "mongoose.h"
 #include <stdio.h>
+
+static const char *g_listening_address = "http://localhost:8000";
+
+static void callback(struct mg_connection *conn, int ev, void *ev_data, void *fn_data)
+{
+	if (ev != MG_EV_HTTP_MSG)
+		return ;
+
+	struct mg_http_message *msg = ev_data;
+	if (mg_http_match_uri(msg, "/api/hello-world"))
+		return mg_http_reply(conn, 200, "Content-Type: application/json\r\n", "{\"result\": %d}\n", 123);
+	printf("data: %s\n", (char *)fn_data);
+	return mg_http_reply(conn, 404, "", "%s", "Not found\n");
+}
 
 int main(void)
 {
-	printf("Hello, world\n");
+	struct mg_mgr mgr;
+	mg_mgr_init(&mgr);
+	mg_http_listen(&mgr, g_listening_address, callback, "hello world");
+	for (;;)
+		mg_mgr_poll(&mgr, 1000);
+	mg_mgr_free(&mgr);
 	return (0);
 }
